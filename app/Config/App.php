043eedb4d592fc0d557d931d2d51ba5376959167
László <habina.laszlo20@gmail.com>
2023-01-06 11:20:51 +0100

    fix

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'barion.develoweb.hu';
+    public $baseURL = 'http://barion.develoweb.hu';
 
     /**
      * --------------------------------------------------------------------------
